ead6f491d8038aa5ac807878dd3ecd1eab572567
David Aragon <david@quickleft.com>
2014-04-17 14:20:30 -0600

    add username route in nodes index

--- routes/nodes.js
+++ routes/nodes.js
@@ -1,12 +1,27 @@
 exports.index = function(req, res) {
 	var lat = req.query.lat;
 	var lon = req.query.lon;
+	var username = req.query.username;
 	var point = {
 		type: "Point", 
 		coordinates: [Number(lon), Number(lat)]
 	};
-	var nodes = req.db.Node.geoNear(point, {maxDistance: 0.5, spherical: true, num:5000}, function(err, results, stats) {
-		console.log(stats)
-		res.send(results);
-	});
+
+	if (lat && lon) {
+		req.db.Node.geoNear(point, {maxDistance: 0.5, spherical: true, num:50000}, function(err, results, stats) {
+			console.log(stats)
+			res.send(results);
+		});	
+	}
+
+	if (username) {
+		req.db.Node.find({'properties.user': username}).limit(200).exec(function(err, node) {
+			res.send(node);
+		});
+	}
+
 };
+
+exports.metadata = function(req, res) {
+	var nodes = req.db.Node.find().sort()
+}
