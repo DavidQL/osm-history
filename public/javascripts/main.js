a377a38013f8ba3167832f132ea4066f0317d24a
David Aragon <david@quickleft.com>
2014-04-19 00:30:55 -0600

    refactoring

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,18 +8,19 @@ var osm = {
 		init: function() {
 			var lat = $('#map').data('lat');
 			var lon = $('#map').data('lon');
-			var map = L.map('map', {
-				markerZoomAnimation: false
-			}).setView([lat, lon], 6);
-
-			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			}).addTo(map);
-
-			this.fetchMetaData(lat, lon, map);
-			// this.fetchNodes(lat, lon, map);
+			var map = osm.map.createNewMap();
+			this.fetchMetaData(lat, lon, map).done(function() {
+				// if date in URL, fetch that immediately
+				if (osm.map.getURLParameter('date')) {
+					(function() {
+						var date = Number(osm.map.getURLParameter('date'));
+						osm.map.fetchNodes(lat, lon, map, date);
+						$('#calendar').datepicker("setDate", moment(date).toDate());
+					})();
+				}
+			});
 		},
-		createNewMap: function(map) {
+		createNewMap: function() {
 			var lat = $('#map').data('lat');
 			var lon = $('#map').data('lon');
 			var map = L.map('map', {
@@ -29,35 +30,24 @@ var osm = {
 			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map);
+			return map;
 		},
 
 		fetchMetaData: function(lat, lon, map) {
-			$.get('/nodes/metadata', function(result) {
+			return $.get('/nodes/metadata', function(result) {
 				var firstDay = moment(result.earliest);
 				var lastDay = moment(result.latest);
 				var cursorDay = moment(result.earliest);
 				var monthsCount = lastDay.diff(firstDay, 'months');
-				// debugger
+
 				$('#calendar').datepicker({
 					numberOfMonths: 3,
 					defaultDate: firstDay.toDate(),
 					onSelect: function(dateText, inst) {
 						var date = moment(dateText).valueOf();
-						// TODO update URL with date param
 						osm.map.fetchNodes(lat, lon, map, date);
 					}
 				});
-
-				if (osm.map.getURLParameter('date')) {
-					(function() {
-						var date = Number(osm.map.getURLParameter('date'));
-						osm.map.fetchNodes(lat, lon, map, date);
-						// TODO select day on calendar
-						console.log('setting date')
-						$('#calendar').datepicker("setDate", moment(date).toDate());
-					})();
-
-				}
 			});
 		},
 		getURLParameter: function(name) {
@@ -65,39 +55,31 @@ var osm = {
 		        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
 		    );
 		},
+		resetMap: function() {
+			var $map = $('#map');
+			$map.clone().empty().insertAfter('#map');
+			$map.remove();
+		},
 		fetchNodes: function(lat, lon, map, date) { // date optional
 			date || (date = osm.map.getURLParameter('date'));
 			$.get('/nodes?lat=' + lat + '&lon=' + lon + (date ? '&date=' + date : '')).done(function(results) {
-				var $map = $('#map');
-				$map.clone().empty().insertAfter('#map');
-				$map.remove();
-				(function() {
-					var lat = $('#map').data('lat');
-					var lon = $('#map').data('lon');
-					var map = L.map('map', {
-						markerZoomAnimation: false
-					}).setView([lat, lon], 6);
+				var map, markers;
+				
+				osm.map.resetMap();
+				map = osm.map.createNewMap();
 
-					L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-					    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					}).addTo(map);
+				$('.results-count').text(results.length);
+				markers = new L.MarkerClusterGroup();
 
-					$('.results-count').text(results.length);
-					var markers = new L.MarkerClusterGroup();
-					window.markers = markers;
-
-
-					_.each(results, function(point, i) {
-						markers.addLayer(new L.marker([point.obj.properties.lat, point.obj.properties.lon], {
-							icon: L.icon({
-								iconUrl: '/images/marker-icon.png',
-							    shadowUrl: '/images/marker-shadow.png'
-							})
-						}));
-					});
-					map.addLayer(markers);
-				})();
-
+				_.each(results, function(point, i) {
+					markers.addLayer(new L.marker([point.obj.properties.lat, point.obj.properties.lon], {
+						icon: L.icon({
+							iconUrl: '/images/marker-icon.png',
+						    shadowUrl: '/images/marker-shadow.png'
+						})
+					}));
+				});
+				map.addLayer(markers);
 			});
 		},
 	}
