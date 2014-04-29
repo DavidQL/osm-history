a920533a6f066adbbd72a18803600e5010feb1df
Nick Aberle <aberle.nick@gmail.com>
2014-04-29 16:59:08 -0600

    added ajax request to bar graph page. added a way cooler loading icon

--- routes/bar_graph.js
+++ routes/bar_graph.js
@@ -2,11 +2,8 @@ var _ = require('underscore');
 
 exports.bar_graph = function(req, res){
   var username = req.params.username;
-  var nodes = req.db.Node.find({'properties.user': username}).limit(200).exec(function(err, node) {
-    res.render('bar_graph', _.extend(res.config, { 
-      data: node, 
-      title: 'D3 Test', 
-      user: username
-    }));
-  });
+  res.render('bar_graph', _.extend(res.config, {  
+    title: 'D3 Test', 
+    user: username
+  }));
 };
